ccf1634e7851bb6c5f0982367d42956f8a6cdbb6
JasonWu <zhesongwu@hotmail.com>
2020-08-02 21:56:56 -0500

    expensify app

---
+++ expensify-app/src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css'; //help works across browsers
+import './styles/styles.scss';
+
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(<p>This is my new app</p>,appRoot);
